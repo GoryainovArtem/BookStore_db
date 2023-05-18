c69bb7cbff2456ac7839101694368655e4001a34
= <=>
2023-05-18 03:36:47 +0300

    ER_diagram

--- SQL_script.sql
+++ SQL_script.sql
@@ -1,3 +1,4 @@
+
 CREATE DATABASE books;
 
 CREATE TABLE IF NOT EXISTS writers (
@@ -53,7 +54,7 @@ CREATE TABLE IF NOT EXISTS series (
 	id_series SERIAL PRIMARY KEY,
 	name_series VARCHAR(100) NOT NULL,
 	description TEXT
-)
+);
 
 
 CREATE TABLE IF NOT EXISTS bindings (
@@ -101,7 +102,7 @@ CREATE TABLE IF NOT EXISTS books_series (
 	FOREIGN KEY (id_book) REFERENCES books (id_book) ON DELETE CASCADE,
 	FOREIGN KEY (id_series_full_info) REFERENCES languages_series_bindings (id_language_series_binding) ON DELETE CASCADE,
 	CONSTRAINT unique_book_series UNIQUE (id_book, id_series_full_info)
-)
+);
 
 
 CREATE TABLE IF NOT EXISTS shops (
@@ -138,7 +139,6 @@ CREATE TABLE IF NOT EXISTS customers (
 );
 
 
-DROP TABLE IF EXISTS delivery_types
 CREATE TABLE IF NOT EXISTS delivery_types (
 	id_delivery_type SERIAL PRIMARY KEY,
 	name_delivery_type VARCHAR(30) NOT NULL UNIQUE
@@ -178,9 +178,8 @@ CREATE TABLE IF NOT EXISTS customers_votes (
 );
 
 
--- Заполнение данными --
+-- Заполнение тестовыми данными --
 -- 1.  Жанры --
-SELECT * FROM genres;
 INSERT INTO genres (name_genre) VALUES
 	('Стихи'),
 	('Роман'),
@@ -197,31 +196,30 @@ INSERT INTO genres (name_genre) VALUES
 	('Историческое');
 
 
--- 2.  Писатели
-SELECT * FROM writers;
+-- 2.  Писатели --
 INSERT INTO  writers (name_writer, birth_date, biography) VALUES
 	('Булгаков М.А.', '1900-01-02'::date, 'Русский писатель советского периода, врач, драматург, театральный режиссёр и актёр. Автор романов, повестей, рассказов, пьес, киносценариев и фельетонов, написанных в 1920-е годы.'),
 	('Пушкин А.С.', '1800-01-01'::date, 'Русский поэт, драматург и прозаик, заложивший основы русского реалистического направления, литературный критик и теоретик литературы, историк, публицист, журналист.'),
 	('Кинг С.', '1943-10-10'::date, 'Американский писатель, работающий в разнообразных жанрах, включая ужасы, триллер, фантастику, фэнтези, мистику, драму, детектив, получил прозвище «Король ужасов». Продано более 350 миллионов экземпляров его книг, по которым было снято множество художественных фильмов и сериалов, телевизионных постановок, а также нарисованы различные комиксы.'),
 	('Маркес Г.Г.', '1927-3-6'::date, 'Колумбийский писатель-прозаик, журналист, издатель и политический деятель. Лауреат Нейштадтской литературной премии и Нобелевской премии по литературе. ');
 	
--- 3.  Издательства
-SELECT * FROM publishers;
+	
+-- 3.  Издательства --
 INSERT INTO publishers (name_publisher) VALUES
 	('Литрес'),
 	('Просвещение'),
 	('Питер'),
 	('АСТ');
 
--- 4. Языки
-SELECT * FROM languages;
+
+-- 4. Языки --
 INSERT INTO languages (name_language) VALUES
 	('Русский'),
 	('Английский'),
 	('Испанский');
 	
--- 5. Серии
-SELECT * FROM series;
+	
+-- 5. Серии --
 INSERT INTO series (name_series, description) VALUES
 	('Эксклюзивная классика', 'В серии «Эксклюзивная классика», выпускаемой в иллюстрированных обложках формата покет, изданы сотни произведений, вошедших в золотой фонд мировой литературы. В их числе — авторы, чьи книги читают на протяжении веков. А именно — Гомер и Данте Алигьери, Джованни Боккаччо и Оноре де Бальзак, Уильям Шекспир и Виктор Гюго, Александр Дюма и Иоганн Вольфганг Гете и другие.'),
 	('Король на все времена', 'В серии «Король на все времена» публикуются книги признанного американского мастера триллера Стивена Кинга. Он пишет в самых разных жанрах: хоррор и фантастика, реализм и фэнтези, драма и мистика. Общий тираж его произведений исчисляется сотнями миллионов. Его работы неоднократно становились бестселлерами в США.'),
@@ -230,22 +228,21 @@ INSERT INTO series (name_series, description) VALUES
 	('Разрыв шаблона. Детектив с шокирующим финалом', 'В этой серии подобраны особенные детективы, финал которых не просто невозможно предугадать, но и шокирует читателя невероятным сюжетным поворотом.'),
 	('100 главных книг', 'Их имена знает каждый человек со школьной скамьи, их творчество стало настоящим откровением для современников и драгоценным посланием потомкам. 100 великих писателей, 100 великих произведений, 100 главных книг. В серии собраны произведения, которые заняли почетное место в списке шедевров мировой литературы и которые должен прочитать каждым мыслящий человек.');
 	
--- 5. Переплеты
-SELECT * FROM bindings;
+	
+-- 6. Переплеты --
 INSERT INTO bindings (name_binding) VALUES
 	('Твердый'),
 	('Мягкий');
 
--- 6. Магазины
-SELECT * FROM shops;
+
+-- 7. Магазины --
 INSERT INTO shops (address, is_online) VALUES
 	('www.books.ru', true),
 	('ул. Ленина, д.23', false),
-	('ул. Москвоская, д.65А', false);
+	('ул. Московская, д.65А', false);
 
 
--- 7. Книги
-SELECT * FROM books;
+-- 8. Книги --
 INSERT INTO books (name_book, writing_date, age_restriction, description) VALUES
 	('Мастер и Маргарита', 1928, 16, 'Роман Михаила Афанасьевича Булгакова, работа над которым началась в декабре 1928 года и продолжалась вплоть до смерти писателя в марте 1940 года. Роман относится к незавершённым произведениям; редактирование и сведение воедино черновых записей осуществляла после смерти мужа вдова писателя - Елена Сергеевна. Первая версия романа, имевшая названия «Копыто инженера», «Чёрный маг» и другие, была уничтожена Булгаковым в 1930 году. В последующих редакциях среди героев произведения появились автор романа о Понтии Пилате и его возлюбленная. Окончательное название - «Мастер и Маргарита» - оформилось в 1937 году. '),
 	('Оно', 1986, 18, 'Роман американского писателя Стивена Кинга, написанный в жанре ужасов, впервые опубликованный в 1986 году издательством Viking Press. В произведении затрагиваются важные для Кинга темы: власть памяти, сила объединённой группы, влияние травм детства на взрослую жизнь. Согласно основной сюжетной линии, семеро друзей из вымышленного города Дерри в штате Мэн сражаются с чудовищем, убивающим детей и способным принимать любую физическую форму, основанные на глубочайших страхах своих жертв. Повествование ведётся параллельно в разных временных интервалах, один из которых соответствует детству главных героев, а другой - их взрослой жизни.'),
@@ -253,8 +250,8 @@ INSERT INTO books (name_book, writing_date, age_restriction, description) VALUES
 	('Сказка о царе Салтане', 1831, 0, 'Сказка в стихах Александра Пушкина, написанная в 1831 году и впервые изданная в следующем году в собрании стихотворений. Сказка посвящена истории женитьбы царя Салтана и рождению его сына, князя Гвидона, который из-за козней тёток попадает на необитаемый остров, встречает там волшебницу - царевну Лебедь, с её помощью становится могущественным владыкой и воссоединяется с отцом. '),
 	('Сто лет одиночества', 1967, 16, 'Роман колумбийского писателя Габриэля Гарсиа Маркеса, одно из наиболее характерных и популярных произведений в направлении магического реализма. Первое издание романа было опубликовано в Буэнос-Айресе в июне 1967 года тиражом 8000 экземпляров. Роман был удостоен премии Ромуло Гальегоса. На сегодняшний день продано более 30 миллионов экземпляров, роман переведён на 35 языков мира.');
 
--- 8. Книга-писатель
-SELECT * FROM books_writers;
+
+-- 9. Книга-писатель --
 INSERT INTO books_writers (id_book, id_writer) VALUES
 	(1, 1),
 	(2, 3),
@@ -262,8 +259,8 @@ INSERT INTO books_writers (id_book, id_writer) VALUES
 	(4, 2),
 	(5, 4);
 
--- 9. Книга-жанр
-SELECT * FROM books_genres;
+
+-- 10. Книга-жанр --
 INSERT INTO books_genres (id_book, id_genre) VALUES
 	(1, 1),
 	(1, 12),
@@ -277,8 +274,8 @@ INSERT INTO books_genres (id_book, id_genre) VALUES
 	(4, 13),
 	(5, 1);
 	
--- 10. Серия-переплет-язык
-SELECT * FROM languages_series_bindings;
+
+-- 11. Серия-переплет-язык --
 INSERT INTO  languages_series_bindings (id_language, id_series, id_binding) VALUES
 	(1, 1, 1),
 	(1, 1, 2),
@@ -289,17 +286,16 @@ INSERT INTO  languages_series_bindings (id_language, id_series, id_binding) VALU
 	(3, 1, 2),
 	(1, 3, 1);
 
--- 11. Серия-издатель
-SELECT * FROM publisher_series;
+
+-- 12. Серия-издатель --
 INSERT INTO publisher_series (id_publisher, id_series) VALUES
 	(1, 3),
 	(2, 2),
 	(3, 1),
-	(4, 5),
-	(5, 4);
+	(4, 5);
 
--- 12. Книга-серия
-SELECT * FROM books_series;
+
+-- 13. Книга-серия --
 INSERT INTO  books_series (id_book, id_series_full_info, isbn, pages_amount, book_weight, published_date) VALUES
 	(1, 1, 1234567891234, 500, 370.0, 2010),
 	(2, 1, 1234567891235, 1300, 500.0, 1998),
@@ -307,65 +303,56 @@ INSERT INTO  books_series (id_book, id_series_full_info, isbn, pages_amount, boo
 	(4, 4, 1234567891237, 200, 250.0, 2016),
 	(5, 6, 1234567891238, 1000, 400.0, 2000);
 
--- 13. Магазины-книги
-SELECT * FROM shops_books;
+
+-- 14. Магазины-книги
 INSERT INTO shops_books (id_book_series, id_shop, default_price, sale, amount, valid_from_dttm) VALUES
 	(1, 1, 400, 0.0, 3, NOW()),
 	(2, 2, 500, 0.0, 2, NOW() - INTERVAL '1' DAY),
 	(3, 2, 600, 7.0, 7, NOW());
 	
-	
--- Тестовая --
 INSERT INTO shops_books (id_book_series, id_shop, default_price, sale, amount, valid_from_dttm, valid_to_dttm) VALUES
-	(1,1, 800, 0.0, 10, '2020-01-01'::TIMESTAMP, '2020-01-31'::TIMESTAMP),
-	(1,1, 1200, 0.0, 10, '2020-02-01'::TIMESTAMP, '2020-02-20'::TIMESTAMP);
+	(1, 1, 800, 0.0, 10, '2020-01-01'::TIMESTAMP, '2020-01-31'::TIMESTAMP),
+	(1, 1, 1200, 0.0, 10, '2020-02-01'::TIMESTAMP, '2020-02-20'::TIMESTAMP);
+	
 
-
--- 14. Покупатели
-SELECT * FROM customers;
+-- 15. Покупатели
 INSERT INTO customers (first_name, middle_name, last_name) VALUES
 	('Артем', 'Константинович', 'Горяинов'),
 	('Андрей', 'Александрович', 'Горлов'),
 	('Данила', 'Вячеславович', 'Тихонов');
 
--- 15. Типы доставки
-SELECT * FROM delivery_types;
+
+-- 16. Типы доставки
 INSERT INTO delivery_types (name_delivery_type) VALUES
 	('Самовывоз'),
 	('Доставка');
 
--- 16. Заказы
-SELECT * FROM purchases;
+
+-- 17. Заказы --
 INSERT INTO purchases (id_customer, id_delivery_type, purchase_dttm, coupon) VALUES
 	(1, 1, NOW(), 0.0),
 	(2, 2, NOW() - INTERVAL '1' DAY, 0.0),
-	(3, 1, NOW(), 15.0);
-
--- Тестовая --
-INSERT INTO purchases (id_customer, id_delivery_type, purchase_dttm, coupon) VALUES
+	(3, 1, NOW(), 15.0),
 	(1, 2, '2020-01-04'::TIMESTAMP, 5),
 	(1, 2, '2020-02-14'::TIMESTAMP, 0);
-SELECT * FROM purchases_detailed;
-INSERT INTO purchases_detailed (id_purchase, id_book_shop, amount) VALUES
-	(4, 4, 2),
-	(5, 5, 1);
 
--- 17. Заказы-подробно
-SELECT * FROM purchases_detailed;
+
+-- 18. Заказы-подробно --
 INSERT INTO purchases_detailed (id_purchase, id_book_shop, amount) VALUES
 	(1, 1, 2),
 	(1, 2, 1),
 	(1, 3, 1),
 	(2, 1, 1),
-	(3, 3, 1);
+	(3, 3, 1),
+	(4, 4, 2),
+	(5, 5, 1);
 
--- 18. Оценки
+-- 19. Оценки --
 SELECT * FROM customers_votes;
 INSERT INTO customers_votes (id_customer, id_book_series, vote) VALUES
 	(1, 1, 7),
 	(1, 2, 4);
 	
-
 
 -- Витрина "Покупки пользователей"--
 CREATE OR REPLACE VIEW customers_purchases_information as (
@@ -424,7 +411,7 @@ buf_4 AS (
 	SELECT id_customer, id_writer, COUNT(*) AS writer_amount FROM buf
 	GROUP BY id_customer, id_writer
 	ORDER BY id_customer, id_writer
-);
+)
 
 SELECT o.* FROM buf_3 o
 LEFT JOIN buf_3 p
@@ -447,6 +434,8 @@ CREATE OR REPLACE VIEW customer_votes_range_by_genres AS (
 	ORDER BY id_customer, avg_vote DESC
 );
 
+SELECT * FROM customer_votes_range_by_genres;
+
 
 -- Витрина "Среднее значение выставленной оценки в зависимости от автора книги" --
 CREATE OR REPLACE VIEW customer_votes_range_by_writers AS (
@@ -457,8 +446,11 @@ CREATE OR REPLACE VIEW customer_votes_range_by_writers AS (
 	ORDER BY id_customer, avg_vote DESC
 );
 
+SELECT * FROM customer_votes_range_by_writers;
+
 
 -- Витрина изначальная стоимость покупки / потраченные деньги для пользователя --
+CREATE OR REPLACE VIEW price_and_paid AS (
 WITH buf AS (
 	SELECT id_purchase, id_customer, sb.default_price, sb.default_price * (1 - 0.01 * sb.sale) AS price_with_sale, pur.coupon, pd.amount FROM purchases pur
 	JOIN purchases_detailed pd USING(id_purchase)
@@ -469,21 +461,28 @@ SELECT id_purchase,
 		SUM(amount * default_price) as expected_price, 
 		SUM(amount * price_with_sale) * (1 - 0.01 * MAX(coupon)) AS paid_money
 FROM buf 
-GROUP BY id_purchase;
+GROUP BY id_purchase
+)
+
+SELECT * FROM price_and_paid;
 
 
 -- Витрина "Продажи магазинов" --
-WITH buf AS (
-	SELECT id_shop, sb.amount AS all_books, pd.amount, purchase_dttm, 
-		EXTRACT(MONTH FROM purchase_dttm) as purchase_month, 
-		EXTRACT(YEAR FROM purchase_dttm) as purchase_year, 
-		sb.default_price * (1 - 0.01 * sb.sale) * (1 - 0.01 * coupon) AS price_with_sale_and_coupon FROM purchases pur
-	JOIN purchases_detailed pd USING(id_purchase)
-	JOIN shops_books sb ON sb.id_book_shop = pd.id_book_shop AND pur.purchase_dttm BETWEEN sb.valid_from_dttm AND sb.valid_to_dttm
+CREATE OR REPLACE VIEW shops_sales AS (
+	WITH buf AS (
+		SELECT id_shop, sb.amount AS all_books, pd.amount, purchase_dttm, 
+			EXTRACT(MONTH FROM purchase_dttm) as purchase_month, 
+			EXTRACT(YEAR FROM purchase_dttm) as purchase_year, 
+			sb.default_price * (1 - 0.01 * sb.sale) * (1 - 0.01 * coupon) AS price_with_sale_and_coupon FROM purchases pur
+		JOIN purchases_detailed pd USING(id_purchase)
+		JOIN shops_books sb ON sb.id_book_shop = pd.id_book_shop AND pur.purchase_dttm BETWEEN sb.valid_from_dttm AND sb.valid_to_dttm
+	)
+	SELECT id_shop, purchase_month, purchase_year, SUM(amount) AS books_sold, SUM(amount * price_with_sale_and_coupon) AS income
+	FROM buf
+	GROUP BY id_shop, purchase_month, purchase_year
 )
-SELECT id_shop, purchase_month, purchase_year, SUM(amount) AS books_sold, SUM(amount * price_with_sale_and_coupon) AS income
-FROM buf
-GROUP BY id_shop, purchase_month, purchase_year;
+
+SELECT * FROM shops_sales;
 
 
 -- Витрина "Динамика цен" --
@@ -515,7 +514,6 @@ WHERE valid_from_dttm <= NOW() AND valid_to_dttm >= NOW() AND sb.amount > 0
 ORDER BY sale DESC;
 
 
-
 -- Функция для получения информации о покупках пользователя --
 CREATE FUNCTION select_user_puchases_info (id INTEGER) 
 RETURNS TABLE(id_customer INTEGER, 
@@ -537,3 +535,26 @@ $$
 $$ LANGUAGE SQL;
 
 SELECT * FROM select_user_puchases_info(1);
+
+-- Все таблицы --
+SELECT * FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE';
+
+DROP TABLE languages CASCADE;
+DROP TABLE languages_series_bindings CASCADE;
+DROP TABLE series CASCADE;
+DROP TABLE bindings CASCADE;
+DROP TABLE publisher_series CASCADE;
+DROP TABLE books_series CASCADE;
+DROP TABLE shops_books CASCADE;
+DROP TABLE shops CASCADE;
+DROP TABLE customers CASCADE;
+DROP TABLE purchases CASCADE;
+DROP TABLE delivery_types CASCADE;
+DROP TABLE purchases_detailed CASCADE;
+DROP TABLE customers_votes CASCADE;
+DROP TABLE publishers CASCADE;
+DROP TABLE books CASCADE;
+DROP TABLE books_writers CASCADE;
+DROP TABLE writers CASCADE;
+DROP TABLE books_genres CASCADE;
+DROP TABLE genres CASCADE;
